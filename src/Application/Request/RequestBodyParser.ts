91f1d376f8196293f2b1dbd9d91f1b2e33fd8180
im-not-enemy <opkd0710@gmail.com>
2020-01-04 23:01:10 +0900

    issue #9 に対応

--- src/Application/Request/RequestBodyParser.ts
+++ src/Application/Request/RequestBodyParser.ts
@@ -2,13 +2,12 @@ export default class RequestBodyParser {
     public run(body:{[key:string]:any}):{[key:string]:any}{
         const newBody:{[key:string]:any}={}
         Object.keys(body).forEach((key)=>{
-            if (key === 'date' || 'startTime' || 'endTime' || 'dayOfWeek'){
+            if (key === 'date' || key === 'startTime' || key === 'endTime' || key === 'dayOfWeek'){
                 newBody[key] = body[key]
             } else {
                 newBody[key] = new RegExp(body[key])
             }
         })
-        console.log(newBody)
         return newBody
     }
 }
