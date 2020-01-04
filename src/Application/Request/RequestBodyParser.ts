78a0461cf3aabcc8f974a16a010ff98fdb98696a
im-not-enemy <opkd0710@gmail.com>
2020-01-04 13:16:13 +0900

    完全一致であれば、数字のフィールドでの検索に対応

--- src/Application/Request/RequestBodyParser.ts
+++ src/Application/Request/RequestBodyParser.ts
@@ -2,8 +2,13 @@ export default class RequestBodyParser {
     public run(body:{[key:string]:any}):{[key:string]:any}{
         const newBody:{[key:string]:any}={}
         Object.keys(body).forEach((key)=>{
-            newBody[key] = new RegExp(body[key])
+            if (key === 'date' || 'startTime' || 'endTime' || 'dayOfWeek'){
+                newBody[key] = body[key]
+            } else {
+                newBody[key] = new RegExp(body[key])
+            }
         })
+        console.log(newBody)
         return newBody
     }
 }
