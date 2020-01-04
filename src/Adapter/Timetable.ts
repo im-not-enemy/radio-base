26060f01893bad31626fcba08e630542ac876972
im-not-enemy <opkd0710@gmail.com>
2020-01-04 12:53:34 +0900

    検索結果をID順でソートするように修正

--- src/Adapter/Timetable.ts
+++ src/Adapter/Timetable.ts
@@ -1,13 +1,11 @@
+import iTimetable from '../Application/TimeTableManagers/iTimetable'
 import nedb from 'nedb'
 import bluebird from 'bluebird'
+const db = new nedb({filename: './database/timetable.db', autoload: true})
+const Cursor = db.find({}).constructor
+// https://github.com/louischatriot/nedb/issues/276#issuecomment-225511866
 bluebird.promisifyAll(nedb.prototype)
-
-import iTimetable from '../Application/TimeTableManagers/iTimetable'
-
-const db = new nedb({
-    filename: './database/timetable.db',
-    autoload: true
-})
+bluebird.promisifyAll(Cursor.prototype)
 
 export default class Timetable implements iTimetable{
     public write(newDoc:{[key:string]:any}):void{
@@ -17,17 +15,17 @@ export default class Timetable implements iTimetable{
     public erase():void{}
 
     public async find(query:{[key:string]:any},option:{[key:string]:any}):Promise<{[key:string]:any}>{
-        return await db.findAsync(query,option)
+        return await db.find(query,option).sort({id:1}).execAsync()
     }
 
     public findByDate():void{}
 
     public async findById(id:number,option:{[key:string]:any}):Promise<{[key:string]:any}>{
-        return await db.findAsync({id:id},option)
+        return await db.find({id:id},option).execAsync()
     }
 
     public async findByStatus(status:string):Promise<{[key:string]:any}>{
-        return await db.findAsync({status:status},{id:1,_id:0})
+        return await db.find({status:status},{id:1,_id:0}).sort({id:1}).execAsync()
     }
 
     public overwrite(id:number,newDoc:{[key:string]:any}):void{
@@ -35,7 +33,7 @@ export default class Timetable implements iTimetable{
     }
 
     public async countByDate(date:number):Promise<number>{
-        return await db.countAsync({date:date})
+        return await db.count({date:date}).execAsync()
     }
     public eraseOldInfo(date:number):boolean{return true}
 }
